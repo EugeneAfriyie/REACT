8bdbdf8a20102a5bd326ae54d6373e589291cbc6
Eugene Afriyie <groupeight00@gmail.com>
2024-12-08 11:17:42 +0000

    beginner projects file

--- LEARNING-BASIC/Beginner-Projects/src/components/Counter.jsx
+++ LEARNING-BASIC/Beginner-Projects/src/components/Counter.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import '../style/counter.css'
+function Counter() {
+  const [count, setCount] = useState(0);
 
-const Counter = () => {
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
-    <div>Counter</div>
-  )
+    <>
+      <div className="container">
+        <h1 className="number">{count}</h1>
+      </div>
+
+      <section className="btns-container">
+        <button onClick={increment} className="increment">
+          +
+        </button>
+        <button onClick={decrement} className="increment">
+          -
+        </button>
+      </section>
+    </>
+  );
 }
 
-export default Counter
+export default Counter;
