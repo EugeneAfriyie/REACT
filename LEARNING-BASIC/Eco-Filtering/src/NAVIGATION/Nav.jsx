e75422817b9a60e3bcd7c699f279693c8fb35679
Eugene Afriyie <groupeight00@gmail.com>
2024-12-09 23:25:07 +0000

    added few things in the nav jsx

--- LEARNING-BASIC/Eco-Filtering/src/NAVIGATION/Nav.jsx
+++ LEARNING-BASIC/Eco-Filtering/src/NAVIGATION/Nav.jsx
@@ -1,7 +1,25 @@
 import './nav.css'
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart,AiOutlineUserAdd } from "react-icons/ai";
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <div className="nav-container">
+        <input type="text" className='search-input' placeholder='Enter Your Product name' />
+      </div>
+
+      <div className="profile-container">
+        <a href="">
+    <FiHeart className='nav-icon'/>
+        </a>
+        <a href="">
+          <AiOutlineShoppingCart className='nav-icon' />
+        </a>
+        <a href="">
+          <AiOutlineUserAdd className='nav-icon' />
+        </a>
+      </div>
+    </nav>
   )
 }
 
