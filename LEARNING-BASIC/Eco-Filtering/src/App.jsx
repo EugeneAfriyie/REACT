cdb03b7aec8cbcb3af3e06b30e981aa1572c0cfe
Eugene Afriyie <groupeight00@gmail.com>
2024-12-09 23:05:52 +0000

    added sidebar folder

--- LEARNING-BASIC/Eco-Filtering/src/App.jsx
+++ LEARNING-BASIC/Eco-Filtering/src/App.jsx
@@ -1,9 +1,13 @@
 import Nav from "./NAVIGATION/Nav"
+import Products from "./Products/Products"
+import Recommended from "./Recommended/Recommended"
 
 const App = () => {
   return (
     <div>
       <Nav/>
+      <Products />
+      <Recommended/>
     </div>
   )
 }
